cc4d5990a51dc6a0e7c08bfb4531eaefcec7755c
kumavis <aaron@kumavis.me>
2022-11-22 21:07:26 -1000

    re-add interactivity

--- packages/voxel-hello-world/index.js
+++ packages/voxel-hello-world/index.js
@@ -47,43 +47,43 @@ module.exports = function(opts, setup) {
 
 function defaultSetup(game, avatar) {
   
-  // var makeFly = fly(game)
-  // var target = game.controls.target()
-  // game.flyer = makeFly(target)
+  var makeFly = fly(game)
+  var target = game.controls.target()
+  game.flyer = makeFly(target)
   
-  // // highlight blocks when you look at them, hold <Ctrl> for block placement
-  // var blockPosPlace, blockPosErase
-  // var hl = game.highlighter = highlight(game, { color: 0xff0000 })
-  // hl.on('highlight', function (voxelPos) { blockPosErase = voxelPos })
-  // hl.on('remove', function (voxelPos) { blockPosErase = null })
-  // hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos })
-  // hl.on('remove-adjacent', function (voxelPos) { blockPosPlace = null })
+  // highlight blocks when you look at them, hold <Ctrl> for block placement
+  var blockPosPlace, blockPosErase
+  var hl = game.highlighter = highlight(game, { color: 0xff0000 })
+  hl.on('highlight', function (voxelPos) { blockPosErase = voxelPos })
+  hl.on('remove', function (voxelPos) { blockPosErase = null })
+  hl.on('highlight-adjacent', function (voxelPos) { blockPosPlace = voxelPos })
+  hl.on('remove-adjacent', function (voxelPos) { blockPosPlace = null })
 
-  // // toggle between first and third person modes
-  // window.addEventListener('keydown', function (ev) {
-  //   if (ev.keyCode === 'R'.charCodeAt(0)) avatar.toggle()
-  // })
+  // toggle between first and third person modes
+  window.addEventListener('keydown', function (ev) {
+    if (ev.keyCode === 'R'.charCodeAt(0)) avatar.toggle()
+  })
 
-  // // block interaction stuff, uses highlight data
-  // var currentMaterial = 1
+  // block interaction stuff, uses highlight data
+  var currentMaterial = 1
 
-  // game.on('fire', function (target, state) {
-  //   var position = blockPosPlace
-  //   if (position) {
-  //     game.createBlock(position, currentMaterial)
-  //   }
-  //   else {
-  //     position = blockPosErase
-  //     if (position) game.setBlock(position, 0)
-  //   }
-  // })
+  game.on('fire', function (target, state) {
+    var position = blockPosPlace
+    if (position) {
+      game.createBlock(position, currentMaterial)
+    }
+    else {
+      position = blockPosErase
+      if (position) game.setBlock(position, 0)
+    }
+  })
 
-  // game.on('tick', function() {
-  //   walk.render(target.playerSkin)
-  //   var vx = Math.abs(target.velocity.x)
-  //   var vz = Math.abs(target.velocity.z)
-  //   if (vx > 0.001 || vz > 0.001) walk.stopWalking()
-  //   else walk.startWalking()
-  // })
+  game.on('tick', function() {
+    walk.render(target.playerSkin)
+    var vx = Math.abs(target.velocity.x)
+    var vz = Math.abs(target.velocity.z)
+    if (vx > 0.001 || vz > 0.001) walk.stopWalking()
+    else walk.startWalking()
+  })
 
 }
--- packages/voxel-hello-world/test.js
+++ packages/voxel-hello-world/test.js
@@ -1,29 +1,4 @@
 require('./')(null, (game, avatar, defaultSetup) => {
-  // const GamePrototype = Reflect.getPrototypeOf(game)
-  // const showChunkSuper = GamePrototype.showChunk
-  // GamePrototype.showChunk = function(chunk) {
-  //   const mesh = showChunkSuper.call(this, chunk)
-  //   var chunkIndex = chunk.position.join('|')
-  //   console.log('chunk index', chunkIndex)
-  //   // var bounds = this.voxels.getBounds.apply(this.voxels, chunk.position)
-  //   // var scale = new THREE.Vector3(1, 1, 1)
-  //   // var mesh = voxelMesh(chunk, this.mesher, scale, this.THREE)
-  //   // this.voxels.chunks[chunkIndex] = chunk
-  //   // if (this.voxels.meshes[chunkIndex]) {
-  //   //   if (this.voxels.meshes[chunkIndex].surfaceMesh) this.scene.remove(this.voxels.meshes[chunkIndex].surfaceMesh)
-  //   //   if (this.voxels.meshes[chunkIndex].wireMesh) this.scene.remove(this.voxels.meshes[chunkIndex].wireMesh)
-  //   // }
-  //   // this.voxels.meshes[chunkIndex] = mesh
-  //   // if (this.isClient) {
-  //   //   if (this.meshType === 'wireMesh') mesh.createWireMesh()
-  //   //   else mesh.createSurfaceMesh(this.materials.material)
-  //   //   this.materials.paint(mesh)
-  //   // }
-  //   // mesh.setPosition(bounds[0][0], bounds[0][1], bounds[0][2])
-  //   // mesh.addToScene(this.scene)
-  //   // this.emit('renderChunk', chunk)
-  //   return mesh
-  // }
   let miniMeshes = {}
   game.on('renderChunk', (chunk) => {
     const chunkIndex = chunk.position.join('|')
